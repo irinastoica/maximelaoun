649be11c80ba429c56a3ab6b14b875cabac210fc
Irina <irinaioanastoica@gmail.com>
2018-04-26 22:44:49 -0400

    changes

--- js/map.js
+++ js/map.js
@@ -4,19 +4,19 @@
                     function init() {
                         // Basic options for a simple Google Map
                         // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-                        var myLatlng = new google.maps.LatLng(28.424653, -81.469516);
+                        var myLatlng = new google.maps.LatLng(45.572116, -73.561704);
 
                         var mapOptions = {
                             // How zoomed in you want the map to start at (always required)
                             zoom: 15,
                             disableDefaultUI: true,
-							scrollwheel: false, 
+							scrollwheel: false,
 
                             // The latitude and longitude to center the map (always required)
 
                             center: myLatlng, // New York
 
-                            // How you would like to style the map. 
+                            // How you would like to style the map.
                             // This is where you would paste any style found on Snazzy Maps.
                             styles: [
     {
@@ -196,20 +196,20 @@
 ]
                         };
 
-                        // Get the HTML DOM element that will contain your map 
+                        // Get the HTML DOM element that will contain your map
                         // We are using a div with id="map" seen below in the <body>
                         var mapElement = document.getElementById('map');
 
                         // Create the Google Map using out element and options defined above
                         var map = new google.maps.Map(mapElement, mapOptions);
-						
+
                         var marker = new google.maps.Marker({
                             position: myLatlng,
                             map: map,
 							icon: 'images/map-marker.png',
                             title: 'Lorem Ipsum'
                         });
-						
-						
+
+
 
                     }
--- js/typed.js
+++ js/typed.js
@@ -21,6 +21,28 @@
 // THE SOFTWARE.
 
 
+$(document).ready(function() {
+  $("#toggle").click(function() {
+    var elem = $("#toggle").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#toggle").text("Read Less");
+      $("#text").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#toggle").text("Read More");
+      $("#text").slideUp();
+    }
+  });
+});
+
+$(document).ready(function() {
+  $("#menu-btn").click(function() {
+      alert
+    }
+  });
+});
+
 
 
 ! function($) {
@@ -429,6 +451,7 @@
         // callback for reset
         resetCallback: function() {}
     };
+
 
 
 }(window.jQuery);
