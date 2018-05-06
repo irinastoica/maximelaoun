0444079e8e4e4dc9a427ce0f39b6cf32a72fd2a9
Irina <irinaioanastoica@gmail.com>
2018-05-06 08:53:08 -0400

    Revert "changes"
    This reverts commit 649be11c80ba429c56a3ab6b14b875cabac210fc.

--- js/typed.js
+++ js/typed.js
@@ -21,28 +21,6 @@
 // THE SOFTWARE.
 
 
-$(document).ready(function() {
-  $("#toggle").click(function() {
-    var elem = $("#toggle").text();
-    if (elem == "Read More") {
-      //Stuff to do when btn is in the read more state
-      $("#toggle").text("Read Less");
-      $("#text").slideDown();
-    } else {
-      //Stuff to do when btn is in the read less state
-      $("#toggle").text("Read More");
-      $("#text").slideUp();
-    }
-  });
-});
-
-$(document).ready(function() {
-  $("#menu-btn").click(function() {
-      alert
-    }
-  });
-});
-
 
 
 ! function($) {
@@ -451,7 +429,6 @@ $(document).ready(function() {
         // callback for reset
         resetCallback: function() {}
     };
-
 
 
 }(window.jQuery);
